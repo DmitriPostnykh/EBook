6e0dd164956e01d71db0683ec9cff8673a7d867d
dmitrijpostnyh <javadeveloper7377@gmail.com>
2024-07-14 16:39:44 -0700

    First commit

--- src/main/java/com/yourlivesound/e_book/controller/HomeController.java
+++ src/main/java/com/yourlivesound/e_book/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
+    @ResponseBody       // all works
     public String home() {
 
         return "Welcome to the E-Book Application";
