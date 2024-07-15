08b123a6d870fe83bb069c69d24cf9bd49efc9f9
dmitrijpostnyh <javadeveloper7377@gmail.com>
2024-07-14 23:36:08 -0700

    Delete @ResponseBody and add home.html and add Thymelef

--- src/main/java/com/yourlivesound/e_book/controller/HomeController.java
+++ src/main/java/com/yourlivesound/e_book/controller/HomeController.java
@@ -1,15 +1,16 @@
 package com.yourlivesound.e_book.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 // first commit
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody       // all works
-    public String home() {
-
-        return "Welcome to the E-Book Application";
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to the E-Book application!");
+        return "home";
     }
 }
